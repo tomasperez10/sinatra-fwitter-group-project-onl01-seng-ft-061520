c9d1001db81c784ebe02dfd65db40465784a220a
Tomas Perez <tomasitopg6@gmail.com>
2020-08-08 05:58:07 +0000

    Automatically backed up by Learn

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ActiveRecord::Base
-  validates :content, presence: true
+    validates :content, presence: true
     validates :user_id, presence: true
     belongs_to :user
     has_many :likes
